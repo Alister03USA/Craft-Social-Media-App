349014ee2b33aa91de0ca1da8e13f30c002bcec9
Alister03USA <81349329+Alister03USA@users.noreply.github.com>
2025-09-04 12:41:31 -0500

    Initial commit for exp1

--- Experiments/Alister/exp2/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/craftsy/CraftsyApplication.java
+++ Experiments/Alister/exp2/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/craftsy/CraftsyApplication.java
@@ -1,4 +1,24 @@
 package coms309.craftsy;
 
+import coms309.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class CraftsyApplication {
+
+    /**
+     * Sample Spring Boot Application.
+     *
+     * @author Alister Gan
+     */
+
+
+        public static void main(String[] args) throws Exception {
+            SpringApplication.run(coms309.Application.class, args);
+        }
+
+
+
 }
--- Experiments/Alister/exp2/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/craftsy/model/User.java
+++ Experiments/Alister/exp2/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/craftsy/model/User.java
@@ -1,4 +1,57 @@
 package coms309.craftsy.model;
 
+import java.util.Arrays;
+
 public class User {
+    private String userName;
+    private String displayName;
+    private String bio;
+    private String profilePicUrl;
+    private String[] craftingSpecialties;
+
+
+    // empty constructor
+    public User(){}
+
+    // Allows creating a user object with all attributes at once
+    public User(String userName, String displayName, String bio, String profilePicUrl, String[] craftingSpecialties) {
+        this.userName = userName;
+        this.displayName = displayName;
+        this.bio = bio;
+        this.profilePicUrl = profilePicUrl;
+        this.craftingSpecialties = craftingSpecialties;
+    }
+
+
+    // Getters and Setters
+    public String getUsername() { return userName; }
+    public void setUsername(String username) { this.userName = username; }
+
+    public String getDisplayName() { return displayName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
+
+    public String getProfilePicUrl() { return profilePicUrl; }
+    public void setProfilePicUrl(String profilePicUrl) { this.profilePicUrl = profilePicUrl; }
+
+    public String[] getCraftingSpecialties() { return craftingSpecialties; }
+    public void setCraftingSpecialties(String[] craftingSpecialties) { this.craftingSpecialties = craftingSpecialties; }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + userName + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", bio='" + bio + '\'' +
+                ", profilePicUrl='" + profilePicUrl + '\'' +
+                ", craftingSpecialties=" + Arrays.toString(craftingSpecialties) +
+                '}';
+    }
+
+
+
+
 }
