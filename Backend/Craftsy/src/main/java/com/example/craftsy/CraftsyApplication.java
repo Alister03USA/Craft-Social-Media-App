c492e732787aa4b7109f36789664dfe9dd7bebd4
Katie Keck <kkeck@iastate.edu>
2025-09-18 21:06:50 -0500

    branch test

--- Backend/Craftsy/src/main/java/com/example/craftsy/CraftsyApplication.java
+++ Backend/Craftsy/src/main/java/com/example/craftsy/CraftsyApplication.java
@@ -3,6 +3,7 @@ package com.example.craftsy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,4 +13,12 @@ public class CraftsyApplication {
         SpringApplication.run(CraftsyApplication.class, args);
     }
 
+}
+
+@RestController
+class HelloController{
+    @GetMapping("/")
+    public String index(){
+        return "Greetings from Spring Boot!";
+    }
 }
