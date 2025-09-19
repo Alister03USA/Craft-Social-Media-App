af261367652f5f5f35b51fe9b1f01e8a6085757b
Katie Keck <kkeck@iastate.edu>
2025-09-18 20:48:55 -0500

    Spring Boot project

---
+++ Backend/Craftsy/src/test/java/com/example/craftsy/CraftsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.craftsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
