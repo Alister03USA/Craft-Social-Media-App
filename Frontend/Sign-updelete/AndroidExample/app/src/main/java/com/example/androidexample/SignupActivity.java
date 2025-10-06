d0d88eaa76d23babf0912d3c227a8ea6a45078d8
jixianfu <jixianfu@MacBook-Air.local>
2025-10-06 11:31:56 -0500

    added confirm password

--- Frontend/Sign-updelete/AndroidExample/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/Sign-updelete/AndroidExample/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -16,7 +16,7 @@ import org.json.JSONObject;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText etUsername, etEmail, etPassword;
+    private EditText etUsername, etEmail, etPassword, etConfirmPassword;
     private Button btnSignup, btnGoToDelete;
 
     // Adjust port based on backend (8080 or 8443 for HTTPS)
@@ -30,6 +30,7 @@ public class SignupActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
         btnSignup = findViewById(R.id.btnSignup);
         btnGoToDelete = findViewById(R.id.btnGoToDelete);
 
@@ -45,12 +46,17 @@ public class SignupActivity extends AppCompatActivity {
         String username = etUsername.getText().toString().trim();
         String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
+        String confirmPassword = etConfirmPassword.getText().toString().trim();
 
-        if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
             return;
         }
-
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_LONG).show();
+            return;
+        }
         JSONObject body = new JSONObject();
         try {
             body.put("username", username);
