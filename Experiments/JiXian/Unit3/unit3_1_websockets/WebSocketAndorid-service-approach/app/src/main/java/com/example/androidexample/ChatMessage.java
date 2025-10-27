b892cc21e82aa37657a09f2e4f1b82ab4d353fb2
jixianfu <jixianfu@MacBook-Air.local>
2025-10-27 17:37:41 -0500

    commit exp changes

---
+++ Experiments/JiXian/Unit3/unit3_1_websockets/WebSocketAndorid-service-approach/app/src/main/java/com/example/androidexample/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.example.androidexample;
+
+public class ChatMessage {
+
+    private String sender;
+    private String message;
+    private boolean isSent;
+
+    public ChatMessage(String sender, String message, boolean isSent) {
+        this.sender = sender;
+        this.message = message;
+        this.isSent = isSent;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isSent() {
+        return isSent;
+    }
+
+    public void setSent(boolean sent) {
+        isSent = sent;
+    }
+}
