5eea7500182c13886d3db5e83815e4e06cc59043
Alister03USA <81349329+Alister03USA@users.noreply.github.com>
2025-11-02 11:33:31 -0600

    Last changes

--- Backend/Craftsy/src/main/java/com/example/craftsy/Group/Controller/GroupController.java
+++ Backend/Craftsy/src/main/java/com/example/craftsy/Group/Controller/GroupController.java
@@ -32,6 +32,7 @@ public class GroupController {
     private NotificationRepository notificationRepository;
 
 
+    // Hello
     /**
      * POST /{adminUsername}/create/
      * This endpoint lets a user create a new group.
