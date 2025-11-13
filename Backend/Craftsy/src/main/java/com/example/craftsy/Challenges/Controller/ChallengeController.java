2b01fd3544202f38a8b374c8823bb6aa6225f6b4
Alister03USA <81349329+Alister03USA@users.noreply.github.com>
2025-11-13 15:43:57 -0600

    Update challenges

--- Backend/Craftsy/src/main/java/com/example/craftsy/Challenges/Controller/ChallengeController.java
+++ Backend/Craftsy/src/main/java/com/example/craftsy/Challenges/Controller/ChallengeController.java
@@ -1,4 +1,19 @@
 package com.example.craftsy.Challenges.Controller;
 
+
+// import com.example.craftsy.PointsSystem.PointsService;
+import com.example.craftsy.SignUpDelete.Entity.Users;
+import com.example.craftsy.SignUpDelete.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+// import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.*;
+
+@RestController
+@RequestMapping("/challenge")
 public class ChallengeController {
+
 }
