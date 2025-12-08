f97b1067c3adbd1e312c1372e76542f5e1216c30
Katie Keck <kkeck@iastate.edu>
2025-12-08 12:20:59 -0600

    fixing feed delete

--- Backend/Craftsy/src/main/java/com/example/craftsy/feed/feedComments/FeedCommentsRepository.java
+++ Backend/Craftsy/src/main/java/com/example/craftsy/feed/feedComments/FeedCommentsRepository.java
@@ -2,10 +2,16 @@ package com.example.craftsy.feed.feedComments;
 
 import com.example.craftsy.feed.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface FeedCommentsRepository extends JpaRepository<FeedComments, Long> {
     List<FeedComments> findByFeedOrderByLikesDesc(Feed feed);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM FeedComments c WHERE c.feed = :feed")
     void deleteAllByFeed(Feed feed);
 }
